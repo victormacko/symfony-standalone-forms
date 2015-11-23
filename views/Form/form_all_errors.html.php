4cb1b3d52ac0ed9d4eb4042f21a69763148bfa36
Victor Macko <victor@victor.com.au>
2015-11-23 17:08:14 +1100

    Additional styling

--- views/Form/form_all_errors.html.php
+++ views/Form/form_all_errors.html.php
@@ -1,10 +1,18 @@
 <?php if (count($errors) > 0): ?>
-<?	if (isset($form['parent']) && $form['parent']) { ?><span class="help-block"><? } else { ?><div class="alert alert-danger"><? } ?>
-	<ul class="list-unstyled">
+<?	$includeContainer = (!isset($include_container) || $include_container === true);	?>
+<?	$list_item_icon = (isset($list_item_icon) ? $list_item_icon : 'glyphicon glyphicon-exclamation-sign');	?>
+<?	$list_class = (isset($list_class) ? $list_class : 'list-unstyled');	?>
+
+<?	if($includeContainer) {
+		if (isset($form['parent']) && $form['parent']) { ?><span class="help-block"><? } else { ?><div class="alert alert-danger"><? }
+	}	?>
+	<ul class="<?= $list_class ?>">
 
 		<?php foreach ($errors as $fieldName => $errorMsg): ?>
-			<li><span class="glyphicon glyphicon-exclamation-sign"></span> <?php echo $fieldName ?> - <?php echo $errorMsg ?></li>
+			<li><? if($list_item_icon !== false) { ?><span class="<?= $list_item_icon ?>"></span> <? } ?><?php echo $fieldName ?> - <?php echo $errorMsg ?></li>
 		<?php endforeach; ?>
 	</ul>
-<?	if (isset($form['parent']) && $form['parent']) { ?></span><? } else { ?></div><? } ?>
+<?	if($includeContainer) {
+		if (isset($form['parent']) && $form['parent']) { ?></span><? } else { ?></div><? }
+	}	?>
 <?php endif ?>
--- web/index.php
+++ web/index.php
@@ -155,4 +155,4 @@ echo 'End: ' . $form_helper->end($formView) . "<br />\n";
 
 echo 'Label: ' . $form_helper->humanize('break_') . "<br />\n";
 echo 'errors (array): ' . print_r($formController->getFormErrorMessagesAsStrings($form, $form_helper), true) . "<hr />\n";
-echo 'errors (html): ' . $formController->getFormErrorMessagesAsHtml($form, $form_helper, $formView) . "<hr />\n";
+echo 'errors (html): ' . $formController->getFormErrorMessagesAsHtml($form, $form_helper, $formView, ['include_container' => false, 'list_item_icon' => false]) . "<hr />\n";
