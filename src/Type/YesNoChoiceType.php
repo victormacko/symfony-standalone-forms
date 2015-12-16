d5065698d67c5ef9a62a8d2bbe838cb061440cbb
Victor Macko <victor@victor.com.au>
2015-12-16 13:54:03 +1100

    Use own choicetype field

--- src/Type/YesNoChoiceType.php
+++ src/Type/YesNoChoiceType.php
@@ -9,7 +9,6 @@
 namespace SymfonySmartyStandaloneForms\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
