6470bdc688fb859b3ff3484bb0ce8ff8aec85792
Victor Macko <victor@victor.com.au>
2016-03-18 00:51:05 +1100

    Minor changes to fix using css-class

--- views/Form/form_widget_simple.html.php
+++ views/Form/form_widget_simple.html.php
@@ -1,5 +1,7 @@
 <?
-$class = isset($class) ? $class : (isset($attr['class']) ? $attr['class'] : '');
+$class = isset($class) ? $class : '';
+$class .= (!empty($attr['class']) ? ' ' . $attr['class'] : '');
+
 if(!isset($type) || 'file' != $type) {
 	$attr['class'] = trim($class . ' ' . $view['form']->block($form, 'form_widget_class'));
 }
@@ -10,9 +12,9 @@ if($inputIcon) {
 	echo $inputIcon;
 }
 ?>
-<input type="<?php echo isset($type) ? $view->escape($type) : 'text' ?>" <?php echo $view['form']->block($form, 'widget_attributes', ['attr' => $attr]) ?><?php if (!empty($value) || is_numeric($value)): ?> value="<?php echo $view->escape($value) ?>"<?php endif ?> />
+	<input type="<?php echo isset($type) ? $view->escape($type) : 'text' ?>" <?php echo $view['form']->block($form, 'widget_attributes', ['attr' => $attr]) ?><?php if (!empty($value) || is_numeric($value)): ?> value="<?php echo $view->escape($value) ?>"<?php endif ?> />
 <?	if($inputIcon) {	?>
-</div>
+	</div>
 <?	}	?>
 <?php if (isset($help)) : ?>
 	<span class="help-block"><?php echo $view->escape($help) ?></span>
