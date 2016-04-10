ed37c1c93f59420de8502045b212cf38c19dce62
Victor Macko <victor@victor.com.au>
2016-04-10 19:29:51 +1000

    Added file type

---
+++ src/Type/FileType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SymfonySmartyStandaloneForms\Type;
+
+use Symfony\Component\Form\Extension\Core\Type\FileType as ParentFileType;
+
+class FileType extends ParentFileType
+{
+	use FormTypeTrait;
+}
